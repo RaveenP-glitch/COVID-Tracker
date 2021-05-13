a551894f2992f9bf59329d35bab70c79389bed9c
Raveen Panditha <raveenpanditha8@gmail.com>
2021-05-14 00:52:18 +0530

    country list

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,29 +1,66 @@
-import React from "react";
-import{
+import React,{useState, useEffect} from "react";
+import {
   MenuItem,
   FormControl,
-  Select,
+  Select
 } from "@material-ui/core";
 import './App.css';
 
 function App() {
+
+const [countries, setCountries] = useState([]);
+
+// https://disease.sh/v3/covid-19/countries
+
+useEffect(() => {
+  const getCountriesData = async() => {
+    await fetch("https://disease.sh/v3/covid-19/countries")
+    .then((response) => response.json())
+    .then((data) => {
+      const countries = data.map((country) => ({
+        
+          name: country.country,  //United States, United Kingdon
+          value: country.countryInfo.iso2 ,  //UK, USA, FR
+        
+      }));
+
+      setCountries(countries);
+    });
+  
+  };
+
+  getCountriesData();
+}, []);
+
   return (
     <div className="App">
-      <h1>COVID-19 Tracker</h1>
-      <FormControl className="app__dropdown">
-        <Select>
-        variant = "outlined"
-        value = "abc"
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
 
-        <MenuItem value="worldwide">Worldwide</MenuItem>
-        <MenuItem value="worldwide">Option two</MenuItem>
-        <MenuItem value="worldwide">Option three</MenuItem>
-        <MenuItem value="worldwide">Option four</MenuItem>
+            {/* Loop through all the countries and show a drop down. */}
+
+            {countries.map((country) => (
+
+            <MenuItem value={country.value}>{country.name}</MenuItem> 
+
+            ))}
 
 
-        </Select>
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option two</MenuItem>
+            <MenuItem value="worldwide">Option three</MenuItem>
+            <MenuItem value="worldwide">Option four</MenuItem> */}
 
-      </FormControl>
+
+          </Select>
+
+        </FormControl>
+
+      </div>
+
+
 
     </div>
   );
