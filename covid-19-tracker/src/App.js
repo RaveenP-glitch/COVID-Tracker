a76177dc4e317548c744fdcaaca11a743229dfab
Raveen Panditha <raveenpanditha8@gmail.com>
2021-05-13 21:52:31 +0530

    npm setup

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,10 +1,30 @@
+import React from "react";
+import{
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>COVID-19 Tracker</h1>
-      
+      <FormControl className="app__dropdown">
+        <Select>
+        variant = "outlined"
+        value = "abc"
+
+        <MenuItem value="worldwide">Worldwide</MenuItem>
+        <MenuItem value="worldwide">Option two</MenuItem>
+        <MenuItem value="worldwide">Option three</MenuItem>
+        <MenuItem value="worldwide">Option four</MenuItem>
+
+
+        </Select>
+
+      </FormControl>
+
     </div>
   );
 }
