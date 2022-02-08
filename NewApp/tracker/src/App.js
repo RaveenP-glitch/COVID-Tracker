5ed237c55202e1bf24c2abd9f7cd830b6d6c54f0
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-02-08 22:26:08 +0530

    folder setup

---
+++ NewApp/tracker/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import styles from './App.module.css';
+
+import { Cards, Chart, CountryPicker } from './components';
+
+class App extends Component {
+  render() {
+    return (
+    <div className={styles.container}>
+        
+        <Cards />
+        <CountryPicker />
+        <Chart />
+
+    </div>
+    );
+  }
+}
+
+export default App;
