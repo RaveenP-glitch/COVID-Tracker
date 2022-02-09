c2707a1ee0de2fd2a2089a9a333d6fdb5f683b03
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-02-10 00:19:07 +0530

    api url

--- NewApp/tracker/src/App.js
+++ NewApp/tracker/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 import { Cards, Chart, CountryPicker } from './components';
 
 class App extends Component {
+
+  componentDidMount(){
+    const data = await fetchData();
+
+    console.log()
+  }
+
   render() {
     return (
     <div className={styles.container}>
