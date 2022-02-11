78f9a5395ee72f4d956daa1ecf0b2642c63f03b0
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-02-11 17:29:38 +0530

    new lines

--- axios-tester/src/App.js
+++ axios-tester/src/App.js
@@ -1,27 +1,17 @@
 import React, {useState,useEffect} from 'react';
-import axios from 'axios';
+
 
 
 function App() {
 
 const [repo, setRepo ] = useState([]);
 
-const getRepo = () => {
-    axios.get('https://www.hpb.health.gov.lk/api/get-current-statistical')
-    .then((response) => {
-        console.log(response);
-        const myRepo = response.data;
-        setRepo(myRepo);
-    })
-};
-
-useEffect(() => getRepo(), []);
 
 
   return (
     <div>
         <p>{repo}</p>
-
+    <h1>Tracker</h1>
     </div>
     
   )
